2dcf5a36d66c03c4f790cb555e7e26e6fb7cf6d5
aravind <flutternoobie>
2022-11-26 21:34:32 +0530

    Third commit

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,7 @@ class MyApp extends StatelessWidget {
     child: Builder(
       builder: (context) {
         return MaterialApp(
+          debugShowCheckedModeBanner: false,
           title: 'Scheduler Demo',
           theme: ThemeData(
             primarySwatch: Colors.blue,
